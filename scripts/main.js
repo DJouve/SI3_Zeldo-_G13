7aebb70feeba00a289d9d79443e8fee461520678
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-04-16 16:05:28 +0200

    sprites

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 let container = document.querySelector(".container")
 let player = document.querySelector(".player")
 let plateau = document.querySelector(".plateau")
-let playerLeft = 0
-let playerTop = 0
+let playerLeft = 45.6
+let playerTop = 33.6
 let step = 2.4
 
-document.onkeydown = animate
+document.onkeydown = animate, morph
 function animate(e){
     // switch(e.keyCode){
     //     case 38 :
@@ -25,6 +25,7 @@ function animate(e){
         if(playerLeft + step < 48){
             playerLeft += step
             player.style.left = playerLeft + 'vw'
+            function switchRight
         }
     }
 
@@ -46,6 +47,11 @@ function animate(e){
         if(playerTop + step < 35){
             playerTop += step
             player.style.top = playerTop + 'vw'
+            for (var i = 0; i = 7; i++) {
+                function morph(){
+                    player.style.backgroundimage = images/
+                }
+            }
         }
     }
 }
