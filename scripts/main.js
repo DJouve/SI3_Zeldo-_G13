6906ad05e85a062a5c7f16bc8aa87dba82852480
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-04-16 16:36:39 +0200

    sprites

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,47 @@
 let container = document.querySelector(".container")
 let player = document.querySelector(".player")
 let plateau = document.querySelector(".plateau")
-let playerLeft = 45.6
-let playerTop = 33.6
+let playerLeft = 0
+let playerTop = 0
 let step = 2.4
+let spriteNumber = 0
 
-document.onkeydown = animate, morph
+function morph() {
+    player.style.backgroundImage = "url('images/facew"+spriteNumber+".png')"
+    spriteNumber++
+    spriteNumber = spriteNumber % 8
+}
+
+document.onkeydown = animate
 function animate(e){
-    // switch(e.keyCode){
-    //     case 38 :
-    //         playerTop -= step
-    //         player.style.top = playerTop + 'px'
-    //     case 40 :
-    //         playerTop += step
-    //         player.style.top = playerTop + 'px'
-    //     case 37 :
-    //         playerLeft -= step
-    //         player.style.left = playerLeft + 'px'
-    //     case 39 :
-    //         playerLeft += step
-    //         player.style.left = playerLeft + 'px'
-    // }
     if(e.keyCode == 39){
-        if(playerLeft + step < 48){
+        if(playerLeft + step < 47){
             playerLeft += step
             player.style.left = playerLeft + 'vw'
-            function switchRight
         }
     }
 
     if(e.keyCode == 37){
-        if(playerLeft >= 0) {
+        if(playerLeft - step >= 0) {
             playerLeft -= step
             player.style.left = playerLeft + 'vw'
         }
     }
 
     if(e.keyCode == 38){
-        if (playerTop >= 0) {
-            playerTop -= step
+        console.log(playerTop - step)
+        if ( (Math.round((playerTop - step) * 10) / 10) >= 0) {
+            playerTop = Math.round((playerTop - step) * 10) / 10
             player.style.top = playerTop + 'vw'
+            morph()
         }
     }
 
     if(e.keyCode == 40){
-        if(playerTop + step < 35){
-            playerTop += step
+        if( (Math.round((playerTop + step) * 10) / 10) < 33){
+            playerTop = Math.round((playerTop + step) * 10) / 10
             player.style.top = playerTop + 'vw'
-            for (var i = 0; i = 7; i++) {
-                function morph(){
-                    player.style.backgroundimage = images/
-                }
-            }
+            morph()
         }
     }
 }
