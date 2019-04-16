398eceb5a494b2c011a4032aefadebe6741a75d6
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-04-16 18:15:07 +0200

    moves complete

--- scripts/main.js
+++ scripts/main.js
@@ -3,21 +3,46 @@ let player = document.querySelector(".player")
 let plateau = document.querySelector(".plateau")
 let playerLeft = 0
 let playerTop = 0
-let step = 2.4
+let step = 0.8
 let spriteNumber = 0
+let spriteGrad = 0
+
+
+function morphStill(){
+    
+
+}
+
+morphStill()
 
 function morph() {
     player.style.backgroundImage = "url('images/facew"+spriteNumber+".png')"
     spriteNumber++
     spriteNumber = spriteNumber % 8
-}
 
+}
+function morphup(){
+    player.style.backgroundImage = "url('images/backw"+spriteNumber+".png')"
+    spriteNumber++
+    spriteNumber =  spriteNumber % 8
+}
+function morphright(){
+    player.style.backgroundImage = "url('images/rightw"+spriteGrad+".png')"
+    spriteGrad++
+    spriteGrad =  spriteGrad % 6
+}
+function morphleft(){
+    player.style.backgroundImage = "url('images/leftw"+spriteGrad+".png')"
+    spriteGrad++
+    spriteGrad =  spriteGrad % 6
+}
 document.onkeydown = animate
 function animate(e){
     if(e.keyCode == 39){
         if(playerLeft + step < 47){
             playerLeft += step
             player.style.left = playerLeft + 'vw'
+            morphright()
         }
     }
 
@@ -25,15 +50,15 @@ function animate(e){
         if(playerLeft - step >= 0) {
             playerLeft -= step
             player.style.left = playerLeft + 'vw'
+            morphleft()
         }
     }
 
     if(e.keyCode == 38){
-        console.log(playerTop - step)
         if ( (Math.round((playerTop - step) * 10) / 10) >= 0) {
             playerTop = Math.round((playerTop - step) * 10) / 10
             player.style.top = playerTop + 'vw'
-            morph()
+            morphup()
         }
     }
 
