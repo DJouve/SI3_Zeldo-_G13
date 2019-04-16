98cf5fb622a74ada897126392bd42226fad95c0e
Damien <44206292+Debilux@users.noreply.github.com>
2019-04-16 15:50:35 +0200

    commit qui ser tàor ein

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 let container = document.querySelector(".plateau")
 let player = document.querySelector(".player")
+let plateau = document.querySelector(".plateau")
 
 let playerLeft = 0
 let playerTop = 0
@@ -40,3 +41,44 @@ function animate(e){
         player.style.top = playerTop + 'px'
     }
 }
+
+function spawn(){
+    let spawn = Math.floor(math.random()*4)
+    if (spawn = 0){
+        
+    }
+}
+
+
+class Monster {
+    constructor(name, life, sprite, taillex, tailley, damages, x, y, range, step, position)
+    {
+        this.sprite = sprite
+        this.taillex = taillex
+        this.tailley = tailley
+        this.name = name
+        this.life = life
+        this.damages = damages
+        this.x = x
+        this.y = y
+        this.range = range
+        this.step = step
+        this.position = position
+        this.death = false
+    }
+    draw ()
+        {
+            this.div = document.createElement('div')
+            this.div.classList.add('.monster')
+
+            plateau.appendChild(this.div)
+
+
+        }
+}
+/*const player = new Monster("player", 100, 2, 10, 10, 10, 400, 400, 5, 5, 1)*/
+
+let soldier = new Monster("soldier", 50, 2, 5, 5, 6, 200, 300, 1, 1 )
+let archer = new Monster("archer", 25, 10, 5, 5, 5, 550, 400, 20, 5, 1)
+
+window.setInterval(archer.draw,5000)
