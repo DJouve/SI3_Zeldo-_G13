e91d3f1d898a1a0bae9b68cf89bb383e46235c8e
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-04-17 18:09:27 +0200

    push

--- scripts/monsters.js
+++ scripts/monsters.js
@@ -1,3 +1,7 @@
+let allMonsters = []
+let spawnNumber = 4
+let monsters = document.querySelector(".monsters")
+let monstersStep = 0.8
 
 class Monster {
     constructor (plateau, monsterLife){
@@ -15,21 +19,55 @@ class Monster {
         plateau.removeChild(this.div)
     }
 
+    moveRight(){
+        this.div.style.left = (parseFloat(this.div.style.left) + monstersStep) + "vw"
+    }
+
+    moveLeft(){
+        this.div.style.left = (parseFloat(this.div.style.left) - monstersStep) + "vw"
+    }
+
+    moveUp(){
+        this.div.style.top = (parseFloat(this.div.style.top) - monstersStep) + "vw"
+    }
+
+    moveDown(){
+        this.div.style.top = (parseFloat(this.div.style.top) + monstersStep) + "vw"
+    }
 }
-
-let allMonsters = []
-let spawnNumber = 4
-
 
 for (let i = 0; i < spawnNumber; i++) {
     let newMonster = new Monster(document.querySelector('.plateau'), 6)
     allMonsters.push(newMonster)
 }
-for (let i = 0; i < allMonsters.length; i++) {
-    if ((isAttackingTop == true) && (allMonsters[i].style.top - player.style.top < 3)){
-        allMonsters[i].life -= 1
-        if (allMonsters[i].life <= 0){
-            allMonsters.remove(allMonsters[i])
+setInterval(function(){
+    for (let i = 0; i < allMonsters.length; i++) {
+        // Gestion attaque
+        if ((isAttackingTop == true) && (allMonsters[i].style.top - playerTop < 10) && (rapportLeft >= -10) && (rapportLeft <= 10)){
+            this.div.monsterLife -= 2
+
+            if (this.div.monsterLife <= 0){
+                allMonsters.splice(i,1)
+                death()
+            }
         }
-    }
-}
+        // Gestion déplacement des monstres
+        let rapportLeft = parseInt(allMonsters[i].div.style.left) - playerLeft
+        let rapportTop = parseInt(allMonsters[i].div.style.top) - playerTop
+        //console.log(rapportTop)
+        //console.log(rapportLeft)
+        // Tu check position relative du monstre par rapport au perso
+        if (rapportTop > 0) {
+            allMonsters[i].moveUp()
+        }
+        if (rapportTop < 0) {
+            allMonsters[i].moveDown()
+        }
+        if (rapportLeft > 0) {
+            allMonsters[i].moveLeft()
+        }
+        if (rapportLeft  < 0) {
+            allMonsters[i].moveRight()
+            }
+        }
+},1000)
