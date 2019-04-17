a893b5fd1f10b1ab768cce11ba5e008a174c724b
Damien <44206292+Debilux@users.noreply.github.com>
2019-04-17 19:01:03 +0200

    :+1: function game

--- scripts/monsters.js
+++ scripts/monsters.js
@@ -1,7 +1,8 @@
 let allMonsters = []
-let spawnNumber = 4
 let monsters = document.querySelector(".monsters")
 let monstersStep = 0.8
+let spawnNumber = 0
+let counter = 0
 
 class Monster {
     constructor (plateau, monsterLife){
@@ -36,10 +37,6 @@ class Monster {
     }
 }
 
-for (let i = 0; i < spawnNumber; i++) {
-    let newMonster = new Monster(document.querySelector('.plateau'), 6)
-    allMonsters = document.querySelectorAll(".monster");
-}
 setInterval(function(){
     for (let i = 0; i < allMonsters.length; i++) {
         // Gestion attaque
@@ -71,3 +68,110 @@ setInterval(function(){
             }
         }
 },1000)
+
+
+function spawn(){
+    console.log("Pute");
+    for (let i = 0; i < spawnNumber; i++) {
+        let newMonster = new Monster(document.querySelector('.plateau'), 6)
+        allMonsters = document.querySelectorAll(".monster");
+        console.log("oui")
+    }
+}
+
+function damage(){
+    for (let i = 0; i < allMonsters.length; i++) {
+        if ((isAttackingTop == true) && (allMonsters[i].style.top - player.style.top < 3)){
+            allMonsters[i].life -= 1
+            if (allMonsters[i].life <= 0){
+                plateau.removeChild(allMonsters[i])
+                counter++
+            }
+        }
+    }
+}
+
+function init(e){
+    button.blur()
+    reset ()
+    game1 ()
+}
+
+let button = document.querySelector("button")
+
+button.addEventListener(
+    'click',
+    init
+)
+
+
+function reset (){
+    do{
+        for (i=0; i<allMonsters.length; i++)
+        plateau.removeChild(allMonsters[i]) // Retreat !
+    } while (allMonsters > 0)
+    playerLeft = 22.4
+    playerTop = 32
+    player.style.top = playerTop + 'vw'
+    player.style.left = playerLeft + 'vw'
+}
+
+
+function game1 (){ // Fonction pour lancer le premier niveau -> pop des monstres
+    spawnNumber = 2
+    // for (i = 0; i<allMonsters.length; i++){
+        window.setInterval(spawn, 4000)
+        damage()
+    
+    if (counter == 14){ // L'idée c'est que quand on tue 15 monstres, on passe au niveau suivant
+        reset ()
+        game2 ()
+    }
+}
+
+function game2 (){// Fonction du niveau 2
+
+    let counter = 0
+    while (counter < 15) {
+        for (i=0; i<1; i++){
+            window.setInterval(archer.draw, 7500)
+            if (archer.life <= 0){
+                plateau.removeChild(archer)
+                counter++
+            }
+            window.setInterval(autremonstre.draw, 12500)
+            if (autremonstre.life <= 0){
+                plateau.removeChild(autremonstre)
+                counter++
+            }
+        }
+    }
+    if (counter == 14){ // Même principe que le niveau 1
+        reset ()
+        game3()
+    }
+}
+
+function game3(){// Fonction du niveau 3
+    
+    while (boss.life>0) {
+        for (i = 0; i<1; i++){// Ca fait apparaitre des mobs au fur et à mesure. Y a plus de retraitre des monstres ils y vont tous
+            window.setInterval(archer.draw, 5000)
+            window.setInterval(soldier.draw, 3500)
+            window.setInterval(autremonstre.draw, 7800)
+            if (archer.life <= 0){
+                plateau.removeChild(archer)
+            }
+            if (soldier.life <= 0){
+                plateau.removeChild(soldier)
+            }
+            if (autremonstre.life <= 0){
+                plateau.removeChild(autremonstre)
+            }
+            boss.draw
+            if (boss.life <= 0){// La win condition c'est de vaincre le boss, vaincre les autres mobs ne fait rien
+                endgame()
+            }
+        }
+    }
+}
